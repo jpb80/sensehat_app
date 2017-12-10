cd1106f81f12c1c8a4be3996bb3db64f39786f4b
Jordan <jpb80@pitt.edu>
2017-12-10 11:16:54 -0500

    initial

---
+++ app.py
@@ -0,0 +1,74 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import sense_hat
+import logging as _log
+import datetime
+
+def _logger_config():
+    _log.basicConfig(level=_log.INFO, format=("[###%(levelname)s] "
+                                                    "%(asctime)s: "
+                                                    "%(filename)s: "
+                                                    "%(funcName)s(): "
+                                                    "%(lineno)d: "
+                                                    "%(message)s"))
+    _log.info("Started")
+
+
+def _convert_to_farenheit(celsius):
+    return celsius * 5 / 9 + 32
+
+
+def _determine_temp_led_color(temp_fahrenheit):
+    result = [255, 0, 250]
+    if temp_fahrenheit >= 95:
+        result = [255, 0 , 0]
+    elif temp_fahrenheit >= 80 and temp_fahrenheit < 95:
+        result = [255, 93, 0]
+    elif temp_fahrenheit >= 76 and temp_fahrenheit < 80:
+        result = [255, 233, 0]
+    elif temp_fahrenheit >= 70 and temp_fahrenheit < 76:
+        result = [118, 255, 0]
+    elif temp_fahrenheit >= 55 and temp_fahrenheit < 70:
+        result = [0, 255, 137]
+    elif temp_fahrenheit >= 40 and temp_fahrenheit < 55:
+        result = [0, 208, 255]
+    elif temp_fahrenheit >= 33 and temp_fahrenheit < 40:
+        result = [0, 110, 255]
+    elif temp_fahrenheit >= 20 and temp_fahrenheit < 32:
+        result = [0, 29, 255]
+    elif temp_fahrenheit >= 0 and temp_fahrenheit < 20:
+        result = [51, 0, 255]
+    return result
+
+
+def _get_local_time():
+    return datetime.datetime.now().strftime("%A, %d. %B %Y %I:%M%p")
+
+def main():
+    _logger_config()
+    sense = sense_hat.SenseHat()
+
+    north = sense.get_compass()
+    print("North: {0:.1f}".format(north))
+
+    humidity = sense.get_humidity()
+    print("Humidity: {0:.1f} %%rH".format(humidity))
+
+    curr_temp = _convert_to_farenheit(sense.get_temperature_from_humidity())
+    print("Local temp: {0:.1f}°F".format(curr_temp))
+
+    curr_pressure = sense.get_pressure()
+    print("Pressure: {0:.4f} Millibars".format(curr_pressure))
+
+    sense.show_message(str(curr_temp),
+        text_colour=_determine_temp_led_color(curr_temp))
+
+    event = sense.stick.wait_for_event(emptybuffer=True)
+    if event.action == stick.ACTION_PRESSED:
+        if event.direction == stick.DIRECTION_MIDDLE:
+            sense.show_message(_get_local_time, text_colour=[255,0,255])
+
+
+if __name__ == '__main__':
+    main()
